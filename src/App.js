ae5527921f1567cf0482db45e62901961ac56f72
Vinay Kushwaha <iamvinayvk@gmail.com>
2022-04-12 23:41:46 +0530

    fixed minor bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import * as faceapi from "face-api.js";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
   const videoRef = useRef(null);
   console.log(faceapi.nets);
   const MODEL_URL = process.env.PUBLIC_URL + "./models";
   const [detect, setDetect] = useState({});
+  const [brk, setBrk] = useState(true);
 
   Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
@@ -31,6 +33,14 @@ function App() {
               .withFaceLandmarks()
               .withFaceExpressions();
             setDetect(detections[0].expressions);
+            setBrk(!brk);
+            // setBrk(
+            //   (Number(detect.angry) * 0.1 +
+            //     Number(detect.disgusted) * 0.2 +
+            //     Number(detect.fearful) * 0.3 +
+            //     Number(detect.sad) * 0.4) /
+            //     4
+            // );
             console.log(detections);
           }, 10);
         });
@@ -41,17 +51,23 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Video</h1>
-      <video ref={videoRef} />
-      <p1>Angry: {detect.angry}</p1>
-      <p1>Disgusted: {detect.disgusted}</p1>
-      <p1>Fearful: {detect.fearful}</p1>
-      <p1>Happy: {detect.happy}</p1>
-      <p1>Neutral: {detect.neutral}</p1>
-      <p1>Sad: {detect.sad}</p1>
-      <p1>Surprised: {detect.surprised}</p1>
-    </div>
+    <>
+      {brk ? (
+        <div className="App">
+          <h1>Video</h1>
+          <video ref={videoRef} />
+          <p1>Angry: {detect.angry}</p1>
+          <p1>Disgusted: {detect.disgusted}</p1>
+          <p1>Fearful: {detect.fearful}</p1>
+          <p1>Happy: {detect.happy}</p1>
+          <p1>Neutral: {detect.neutral}</p1>
+          <p1>Sad: {detect.sad}</p1>
+          <p1>Surprised: {detect.surprised}</p1>
+        </div>
+      ) : (
+        <Message />
+      )}
+    </>
   );
 }
 
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = () => {
+  return <div className="App">Please Take Break</div>;
+};
+
+export default Message;
