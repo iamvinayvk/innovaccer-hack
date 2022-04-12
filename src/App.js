0161a7eea2211f9c95b19fc0d8e7371b34221be2
Vinay Kushwaha <iamvinayvk@gmail.com>
2022-04-12 22:51:41 +0530

    intitial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import * as faceapi from "face-api.js";
+import "./App.css";
 
 function App() {
+  const videoRef = useRef(null);
+  console.log(faceapi.nets);
+  const MODEL_URL = process.env.PUBLIC_URL + "./models";
+  const [detect, setDetect] = useState({});
+
+  Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+    faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+    faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+    faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+  ]);
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: {} })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play().then(() => {
+          setInterval(async () => {
+            const detections = await faceapi
+              .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+              .withFaceLandmarks()
+              .withFaceExpressions();
+            setDetect(detections[0].expressions);
+            console.log(detections);
+          }, 10);
+        });
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Video</h1>
+      <video ref={videoRef} />
+      <p1>Angry: {detect.angry}</p1>
+      <p1>Disgusted: {detect.disgusted}</p1>
+      <p1>Fearful: {detect.fearful}</p1>
+      <p1>Happy: {detect.happy}</p1>
+      <p1>Neutral: {detect.neutral}</p1>
+      <p1>Sad: {detect.sad}</p1>
+      <p1>Surprised: {detect.surprised}</p1>
     </div>
   );
 }
